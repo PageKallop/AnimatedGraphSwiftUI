b657d82f05a77bf89b177f6d7bcb7eb152ba010c
PageKallop <PageMarie78@gmail.com>
2021-01-21 14:29:13 -0600

    Added drag animation to card

--- AnimatedGraphSwiftUI/ContentView.swift
+++ AnimatedGraphSwiftUI/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var tapped: Bool = false
+    @State private var cardDragState = CGSize.zero
+    
+    
     var body: some View {
         VStack{
         //implements Bar graph 
         BarGraph(reports: ReportModel.all())
            
             Card(tapped: self.tapped)
+                .animation(.easeInOut)
+                .offset(y: self.cardDragState.height)
+                .gesture(DragGesture()
+                            .onChanged { value in
+                                self.cardDragState = value.translation
+                            }
+                            .onEnded {
+                                value in
+                                self.cardDragState = CGSize.zero
+                            }
+                )
+                
                 .gesture(TapGesture(count: 1)
                         .onEnded({ () in
                             self.tapped.toggle()
